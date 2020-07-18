fcf1fbaf35c4369b89ba0f5d72e32e65691c1817
Jacob Viti <thevitij64@gmail.com>
2020-07-18 03:35:16 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -11,7 +11,7 @@ require 'pry'
 def get_the_values(groceries)
 groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
 
-return groceries.values
+return groceries.keys
 #  => ["Banana", "Broccoli", "Cookie"]
 
 end
